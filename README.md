f741a3156c669bda2b9fe9583d46ce9a6470b0ac
Marc <wangchengda1990@gmail.com>
2017-09-07 21:25:23 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# BubbleViewDemo
+# BubbleView
+
+**一个可以让所有的控件进行拖动的工具，使用简单，只需要一行代码就能让你的控件变得可以拖动起来！**
+
+## 引入
+
+在 build.gradle 中配置:
+
+```
+repositories {
+    jcenter() // If not already there
+}
+
+dependencies {
+    compile 'marc.com:bubbleview:1.1'
+}
+```
+
+## 使用
+
+```Java
+BubbleView.attach(findViewById(R.id.bubble_text), new BubbleView.BubbleDisappearListner() {
+   @Override
+   public void disappear(View view) {
+      Toast.makeText(MainActivity.this, view.getId() +" disappeared!", Toast.LENGTH_SHORT).show();
+   }
+});
+```
--- app/src/main/java/marc/com/bubbleviewdemo/MainActivity.java
+++ app/src/main/java/marc/com/bubbleviewdemo/MainActivity.java
@@ -1,25 +1,36 @@
 package marc.com.bubbleviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import marc.com.bubbleview.BubbleView;
 
-public class MainActivity extends AppCompatActivity implements BubbleView.BubbleDisappearListner {
+public class MainActivity extends AppCompatActivity  {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		BubbleView.attach(findViewById(R.id.bubble_text),this);
-		BubbleView.attach(findViewById(R.id.bubble_btn),this);
-		BubbleView.attach(findViewById(R.id.bubble_img),this);
+		BubbleView.attach(findViewById(R.id.bubble_text), new BubbleView.BubbleDisappearListner() {
+			@Override
+			public void disappear(View view) {
+				Toast.makeText(MainActivity.this, view.getId() +" disappeared!", Toast.LENGTH_SHORT).show();
+			}
+		});
+		BubbleView.attach(findViewById(R.id.bubble_btn),new BubbleView.BubbleDisappearListner() {
+			@Override
+			public void disappear(View view) {
+
+			}
+		});
+		BubbleView.attach(findViewById(R.id.bubble_img),new BubbleView.BubbleDisappearListner() {
+			@Override
+			public void disappear(View view) {
+
+			}
+		});
 	}
 
-	@Override
-	public void disappear(View view) {
-		Toast.makeText(this, view.getId() + " disappeared!", Toast.LENGTH_SHORT).show();
-	}
 }
